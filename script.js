0f62474a0baaf7b73366014881884f261faaf117
vidhivdubey <vidhivdubey@gmail.com>
2026-06-11 02:55:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+}
